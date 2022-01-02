c54638fd9f1427d7b16d18c35eba82b018024410
TeaPot98 <trifautan@gmail.com>
2022-01-02 02:18:53 +0200

    finished part 4a !!!

---
+++ tests/blog_api.test.js
@@ -0,0 +1,56 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('returned expected number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('successfully created new blog', async () => {
+    const newBlog = {
+        title: 'simple blog',
+        author: 'John Doe',
+        url: 'url',
+        likes: 15
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain(
+        'simple blog'
+    )
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
