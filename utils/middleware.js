e8a4f33d799ae468e10894ef42e4607ad9125cd6
TeaPot98 <trifautan@gmail.com>
2022-01-27 00:16:55 +0200

    finished part5d

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,6 +2,14 @@ const jwt = require('jsonwebtoken')
 const logger = require('./logger')
 const User = require('../models/user')
 
+const requestLogger = (request, response, next) => {
+    logger.info('Method:', request.method)
+    logger.info('Path:  ', request.path)
+    logger.info('Body:  ', request.body)
+    logger.info('---')
+    next()
+}
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({
         error: 'unknown endpoint'
@@ -25,7 +33,7 @@ const errorHandler = (error, request, response, next) => {
         })
     } else if (error.name === 'TokenExpiredError') { 
         return response.status(401).json({
-            error: 'token epired'
+            error: 'token expired'
         })
     }
 
@@ -67,6 +75,7 @@ const userExtractor = async (request, response, next) => {
 
 module.exports = {
     unknownEndpoint,
+    requestLogger,
     errorHandler,
     tokenExtractor,
     userExtractor
